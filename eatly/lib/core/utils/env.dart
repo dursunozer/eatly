7c1097077159d89a826d71ad808ef725b4554e26
ozer <dursun669@outlook.com>
2025-09-12 13:41:04 +0300

    backend added

--- eatly/lib/core/utils/env.dart
+++ eatly/lib/core/utils/env.dart
@@ -1,7 +1,7 @@
 class EnvConfig {
   // Cloud Functions HTTPS endpoint URL (proxy). Örn:
   // https://<region>-<project>.cloudfunctions.net/visionProxy
-  static const String visionProxyEndpoint = '';
+  static const String visionProxyEndpoint = 'http://127.0.0.1:8000/api/vision/analyze';
 }
 
 
